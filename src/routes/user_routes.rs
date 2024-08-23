0b8925ca6978d7a6f923e9092c64c877e72eaccf
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2024-08-23 23:23:40 +0200

    Add sign-in route

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -4,4 +4,5 @@ use actix_web::web;
 pub fn configure_user_routes(cfg: &mut web::ServiceConfig) {
     //cfg.service(web::resource("/users").route(web::get().to(user_controller::get_all_users)));
     cfg.service(web::resource("/sign-up").route(web::post().to(user_controller::sign_up)));
+    cfg.service(web::resource("/sign-in").route(web::post().to(user_controller::sign_in)));
 }
