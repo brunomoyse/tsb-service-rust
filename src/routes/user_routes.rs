846d938829e08b32993bdbd28bf43f64bd4e7ccb
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2024-08-24 23:04:35 +0200

    Add refresh token route

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -5,4 +5,6 @@ pub fn configure_user_routes(cfg: &mut web::ServiceConfig) {
     //cfg.service(web::resource("/users").route(web::get().to(user_controller::get_all_users)));
     cfg.service(web::resource("/sign-up").route(web::post().to(user_controller::sign_up)));
     cfg.service(web::resource("/sign-in").route(web::post().to(user_controller::sign_in)));
+    cfg.service(web::resource("/refresh-token").route(web::post().to(user_controller::refresh_token)));
+
 }
